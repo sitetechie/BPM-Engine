4f52d55a298fdbec0441155aa08456b49ff0f7d1
sitetechie <techie@sitetechie.com>
2011-03-25 03:53:02 +0100

    Add ProcessRunner traits. Add POD documentation.

--- lib/BPM/Engine.pm
+++ lib/BPM/Engine.pm
@@ -5,48 +5,52 @@ BEGIN {
     }
 
 use 5.010;
+use namespace::autoclean;
 use Moose;
 use MooseX::StrictConstructor;
+use Scalar::Util ();
 use BPM::Engine::Exceptions qw/throw_engine/;
 use BPM::Engine::Store;
 use BPM::Engine::ProcessRunner;
-use namespace::autoclean;
+use BPM::Engine::Types qw/ArrayRef/;
 
 with qw/
     MooseX::SimpleConfig
-    MooseX::Traits    
+    MooseX::Traits
     BPM::Engine::Role::WithCallback
     BPM::Engine::Role::WithPersistence
-    BPM::Engine::Role::WithLogger    
+    BPM::Engine::Role::WithLogger
     BPM::Engine::Handler::ProcessDefinitionHandler
     BPM::Engine::Handler::ProcessInstanceHandler
     BPM::Engine::Handler::ActivityInstanceHandler
     /;
 with 'BPM::Engine::Role::EngineAPI';
 
-has '+configfile' => ( default => '/etc/bpmengine.yaml' );
+#has '+configfile'       => ( default => '/etc/bpmengine/engine.yaml' );
 
-around BUILDARGS => sub {
-    my $orig = shift;
-    my $args = $orig->(@_);
+has '+_trait_namespace' => ( default => 'BPM::Engine::Trait' );
 
-    throw_engine("Invalid connection arguments") 
-        unless $args->{connect_info} || $args->{schema};
-
-    return $args;
-    };
+has 'runner_traits' => (
+    isa       => ArrayRef,
+    is        => 'rw',
+    default   => sub { [] },
+    predicate => 'has_runner_traits'
+    );
 
 sub runner {
     my ($self, $pi) = @_;
-    
+
+    Scalar::Util::weaken($self);
+
     my $args = {
         process_instance => $pi,
         engine           => $self,
         logger           => $self->logger,
         };
     $args->{callback} = $self->callback if $self->has_callback;
-    
-    return BPM::Engine::ProcessRunner->new($args);
+    $args->{traits}   = $self->runner_traits if $self->has_runner_traits;
+
+    return BPM::Engine::ProcessRunner->new_with_traits($args);
     }
 
 __PACKAGE__->meta->make_immutable;
@@ -60,7 +64,7 @@ __END__
 
 =head1 NAME
 
-BPM::Engine - Business process execution engine
+BPM::Engine - Business Process Execution Engine
 
 =head1 VERSION
 
@@ -68,28 +72,39 @@ BPM::Engine - Business process execution engine
 
 =head1 SYNOPSIS
 
-  use BPM::Engine;
+Create a new bpm engine  
   
+  use BPM::Engine;
+
   my $callback = sub {
         my($runner, $entity, $event, $node, $instance) = @_;
         ...
         };
-  
+
   my $engine = BPM::Engine->new(
       log_dispatch_conf => 'log.conf',
       connect_info      => { dsn => $dsn, user => $user, password => $password },
       callback          => $callback
       );
+
+Save an XPDL file with workflow process definitions, and retrieve the process 
+definitions
   
   my $package = $engine->create_package('/path/to/model.xpdl');
 
-  my @workflows = $engine->list_process_definitions->all;
+  my @processes = $engine->get_process_definitions->all;
+
+Create and run a process instance  
   
   my $instance = $engine->create_process_instance(
       $process, 'Client xyz', { param1 => 'value1' }
       );
-  
+
   $engine->start_process_instance($instance);
+
+=head1 DISCLAIMER
+
+This is ALPHA SOFTWARE. Use at your own risk. Features may change.
 
 =head1 DESCRIPTION
 
@@ -97,83 +112,205 @@ BPM::Engine is an embeddable workflow process engine with persistence. It
 handles saving and loading XPDL packages in a database, and running workflow
 processes.
 
-=head1 WARNING
-
-Currently this is B<alpha code> and should be considered B<HIGHLY
-EXPERIMENTAL>. This module is still in heavy flux, and will change. I welcome 
-any opinions, ideas for extensions, etc. However, tests are incomplete, 
-documentation is nonexistent, and interfaces are subject to change. 
-B<Don't use this in production>.
-
-Please review the test files and source code to see how it works.
-
 =head1 INTERFACE
 
-=head2 CONSTRUCTORS
+=head2 Constructors
 
-=head2 new
+=head3 B<< BPM::Engine->new(%options) >>
 
-=head2 new_with_config
+Creates a new bpm engine.
 
-=head2 ATTRIBUTES
+    $engine = BPM::Engine->new(
+      connect_info => {
+        dsn      => $dsn,
+        user     => $user,
+        password => $pass,
+        %dbi_attributes,
+        %extra_attributes,
+        });
 
-=head2 connect_info
+Possible options are:
 
-=head2 schema
+=over
 
-=head2 storage
+=item C<< schema => $schema // BpmEngineStore >>
 
-=head2 logger
+L<BPM::Engine::Store> connected schema object. If not provided, one will be 
+created using the C<connect_info> option.
 
-=head2 log_dispatch_conf
+Either C<schema> or C<connect_info> is required on object construction.
 
-=head2 callback
+=item C<< connect_info => $dsn // ConnectInfo >>
+
+DBIx::Class::Schema connection arguments that get passed to the C<connect()>
+call to BPM::Engine::Store, as specified by the C<ConnectInfo> type in
+L<BPM::Engine::Types::Internal>.
+
+Usually a single hashref with dsn/user/password and attributes.
+
+This attribute is only used to build the C<schema> attribute if not provided
+already.
+
+=item C<< logger => $logger // BpmEngineLogger >>
+
+A logger object that implements the L<BPM::Engine::Role::LoggerAPI> role,
+defaults to a L<BPM::Engine::Logger> instance constructed with
+C<log_dispatch_conf>.
+
+=item C<< log_dispatch_conf => $file | $hashref >>
+
+Optional constructor argument for L<BPM::Engine::Logger> to build the default
+C<logger>, if a logger was not provided.
+
+=item C<< callback => \&cb >>
+
+Optional callback I<&cb> which is called on all process instance events. This 
+option is passed to any C<BPM::Engine::ProcessRunner> constructor.
+
+=item C<< runner_traits => [qw/TraitA TraitB/] // [] >>
+
+Optional traits to be supplied to all C<BPM::Engine::ProcessRunner> objects used.
+
+=back
+
+=head3 B<< BPM::Engine->new_with_config(%options) >>
+
+    $engine = BPM::Engine->new_with_config(
+      configfile => "/etc/bpmengine/engine.conf"
+      );
+
+Provided by the base role L<MooseX::SimpleConfig>.  Acts just like
+regular C<new()>, but also accepts an argument C<configfile> to specify
+the configfile from which to load other attributes. 
+
+=over
+
+=item C<< configfile => $file // $ENV{HOME}/bpmengine/engine.conf >>
+
+A file that, when passed to C<new_with_config>, is parsed using
+L<Config::Any> to support any of a variety of different config formats,
+detected by the file extension.  See L<Config::Any> for more details
+about supported formats.
+
+=back
+
+Explicit arguments to C<new_with_config> will override anything loaded from the 
+configfile.
+
+=head3 B<< BPM::Engine->new_with_traits(%options) >>
+
+Just like C<new()>, but also accepts a C<traits> argument with a list of trait 
+names to apply to the engine object.
+
+    $engine = BPM::Engine->new_with_traits(
+        traits => [qw/Foo Bar/],
+        schema => $schema
+        );
+
+Options, in addition to those to C<new()>:
+
+=over
+
+=item C<< traits => \@traitnames // [] >>
+
+Traits live under the C<BPM::Engine::Trait> namespace by default, prefix full 
+class names with a C<+>.
+
+=back
+
+=head3 B<< BPM::Engine->with_traits(@traits)->new(%options) >>
+    
+You can use the C<with_traits> class method to use traits in combination
+with a configuration file. Example:
+
+    $engine = BPM::Engine->with_traits(qw/Foo Bar/)->new_with_config(
+      configfile => '/home/user/bpmengine.conf'
+      );
 
 =head2 PROCESS DEFINITION METHODS
 
-=head2 list_packages
+=head3 get_packages
 
-=head2 create_package
+    $rs = $engine->get_packages();
 
-=head2 delete_package
+=head3 get_package
 
-=head2 list_process_definitions
+    $package = $engine->get_package($package_id);
 
-=head2 get_process_definition
+=head3 create_package
+
+    $package = $engine->create_package($file);
+
+=head3 delete_package
+
+    $engine->delete_package($package_id);
+
+=head3 get_process_definitions
+
+    $rs = $engine->get_process_definitions();
+
+=head3 get_process_definition
+
+    $process = $engine->get_process_definition
 
 =head2 PROCESS INSTANCE METHODS
 
-=head2 list_process_instances
+=head3 get_process_instances
 
-=head2 create_process_instance
+    $rs = $engine->get_process_instances();
 
-=head2 get_process_instance
+=head3 get_process_instance
 
-=head2 start_process_instance
+    $process_instance = $engine->get_process_instance($pi_id);
 
-=head2 terminate_process_instance
+=head3 create_process_instance
 
-=head2 abort_process_instance
+    $process_instance = $engine->create_process_instance($process_id);
 
-=head2 delete_process_instance
+=head3 start_process_instance
 
-=head2 process_instance_attribute
+    $engine->start_process_instance($pi_id);
 
-=head2 change_process_instance_state
+=head3 terminate_process_instance
+
+=head3 abort_process_instance
+
+=head3 delete_process_instance
+
+=head3 process_instance_attribute
+
+=head3 change_process_instance_state
 
 =head2 ACTIVITY INSTANCE METHODS
 
-=head2 list_activity_instances
+=head3 get_activity_instances
 
-=head2 get_activity_instance
+=head3 get_activity_instance
 
-=head2 change_activity_instance_state
+=head3 change_activity_instance_state
 
-=head2 activity_instance_attribute
+=head3 activity_instance_attribute
 
-=head2 LOGGING
+=head2 LOGGING METHODS
 
 log, debug, info, notice, warning, error, critical, alert, emergency
+
+=head2 INTERNAL METHODS
+
+=head3 runner
+
+Returns a new L<BPM::Engine::ProcessRunner> instance with the C<runner_traits>
+and C<callback> attribute applied for the specified process instance.
+Internal method, used by C<start_process_instance()> and
+C<change_activity_instance_state()> to advance a process instance.
+
+=head1 DIAGNOSTICS
+
+=head2 Exception Handling
+
+When C<BPM::Engine> encounters an API error, it throws a
+C<BPM::Engine::Exception> object.  You can catch and process these exceptions,
+see L<BPM::Engine::Exception> for more information.
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
@@ -195,7 +332,11 @@ example.
 
 =item * Template Toolkit
 
+=item * Text::Xslate
+
 =item * XML::LibXML
+
+=item * Graph
 
 =back
 
@@ -217,11 +358,10 @@ You can contribute or fork this project via GitHub:
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-bpm-engine@rt.cpan.org>,
-or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=BPM-Engine>. I will be
-notified, and then you'll automatically be notified of progress on your bug as
-I make changes.
+Plenty. Along with error conditions not being handled gracefully etc.
+
+They will be fixed in due course as I start using this more seriously,
+however in the meantime, patches are welcome :)
 
 =head1 SUPPORT
 
@@ -245,10 +385,10 @@ L<http://github.com/sitetechie/BPM-Engine>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2010, Peter de Vos C<< <sitetech@cpan.org> >>.
+Copyright (c) 2010, 2011 Peter de Vos.
 
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
+This module is free software; you can redistribute it and/or modify it under the 
+same terms as Perl itself. See L<perlartistic>.
 
 =head1 DISCLAIMER OF WARRANTY
 
